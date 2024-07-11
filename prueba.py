b8e92dcfe72adf6a067a430717a9b844f3a9a485
Vina del Mar <vina@duoc.acad>
2024-07-11 10:21:43 -0400

    Funciona requisito uno y dos

--- prueba.py
+++ prueba.py
@@ -1,4 +1,5 @@
 import csv
+import math
 var_menu = True
 
 def menu():
@@ -7,10 +8,34 @@ def menu():
     print("3)Cantidad por año")
     print("4)Salir")
 
-def filtrar_archivo():
+def filtrar_archivo(filtro):
     with open('juegos.csv',mode="r",newline='') as archivo:
         data = csv.reader(archivo)
-        print(data)
+        cadena_guardar =""
+        for juego in data:
+            if filtro.capitalize() in juego[0]:
+                cadena_guardar += juego[0]+ "------>"+juego[-2] +"\n" 
+        
+        print(cadena_guardar)
+
+        with open("filtro_por_nombre.txt","w") as archivo_a_guardar:
+            archivo_a_guardar.write(cadena_guardar)
+
+def promedio_precio_consola(consola):
+
+    with open('juegos.csv',mode="r",newline='') as archivo:
+        data = csv.reader(archivo)
+        precio_juegos =0.0
+        contador=0
+        for juego in data:
+            if consola.capitalize() in juego[3]:
+                precio_juegos += float(juego[2])
+                contador +=1
+
+        promedio = precio_juegos/contador
+        print(f"EL precio promedio de los juegos de la consola X360 es", promedio.__round__(2))
+        
+     
     
 while var_menu:
     menu()
@@ -20,8 +45,9 @@ while var_menu:
         var_menu = False
     elif opcion =="1":
         filtrado = input("Ingrese una palabra clave para filtrar por nombre")
-        filtrar_archivo()
+        filtrar_archivo(filtrado)
     elif opcion == "2":
         consola  = input("Ingrese el nombre de una consola para obtener el promedio")
+        promedio_precio_consola(consola)
     elif opcion =="3":
         print("tercera")
