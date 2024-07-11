a8c02ccecaec4e51a1e9ba532be3ededcb3e99b2
Vina del Mar <vina@duoc.acad>
2024-07-11 11:01:37 -0400

    terminando tercer requisito

--- prueba.py
+++ prueba.py
@@ -15,11 +15,14 @@ def filtrar_archivo(filtro):
         for juego in data:
             if filtro.capitalize() in juego[0]:
                 cadena_guardar += juego[0]+ "------>"+juego[-2] +"\n" 
-        
-        print(cadena_guardar)
 
-        with open("filtro_por_nombre.txt","w") as archivo_a_guardar:
-            archivo_a_guardar.write(cadena_guardar)
+        if len(cadena_guardar) != 0:
+            with open("filtro_por_nombre.txt","w") as archivo_a_guardar:
+                archivo_a_guardar.write(cadena_guardar)
+            return True
+        else:
+            
+            return False
 
 def promedio_precio_consola(consola):
 
@@ -31,9 +34,54 @@ def promedio_precio_consola(consola):
             if consola.capitalize() in juego[3]:
                 precio_juegos += float(juego[2])
                 contador +=1
+        if contador !=0:
+            promedio = precio_juegos/contador
+            print(f"EL precio promedio de los juegos de la consola X360 es", promedio.__round__(2))
+            return True
+        else:
+            return False
 
-        promedio = precio_juegos/contador
-        print(f"EL precio promedio de los juegos de la consola X360 es", promedio.__round__(2))
+def filtrar_juegos_por_anio(anios):
+    with open('juegos.csv',mode="r",newline='') as archivo:
+        data = csv.reader(archivo)
+        contadorpsps=0
+        contadorx360=0
+        contadorDs=0
+        contadorps3=0
+        contadorWii=0
+        cadena_a_guardar =""
+        consolas = []
+        for juego in data:
+            if anios == juego[-1]:
+                consolas.append(juego[-2])
+                len(juego[-2])
+
+ 
+        for conso in consolas:
+            if conso == "Sony PSP":
+                contadorpsps+=1
+            if conso =='X360':
+                contadorx360+=1
+            if conso == 'Nintendo DS':
+                contadorDs+=1
+            if conso =='PlayStation 3':
+                contadorps3+=1
+            if conso =='Nintendo Wii':
+                contadorWii+=1
+            
+        if contadorDs >0 :
+            cadena_a_guardar += (f"La consola  nintendo ds tuvo {contadorDs} juegos\n")
+        if contadorpsps >0:
+            cadena_a_guardar += (f"La consola  psp tuvo {contadorpsps} juegos \n")
+        if contadorx360 >0:
+            cadena_a_guardar += (f"La consola  xbox360 tuvo {contadorx360} juegos \n")
+        if contadorps3 >0:
+            cadena_a_guardar += (f"La consola  playstation 3 tuvo {contadorps3} juegos \n")
+        if contadorWii >0:
+            cadena_a_guardar += (f"La consola  nintendo wii tuvo {contadorWii} juegos \n")
+
+        with open(f"juegos_{anios}","w") as arch:
+            arch.write(cadena_a_guardar)
         
      
     
@@ -45,9 +93,25 @@ while var_menu:
         var_menu = False
     elif opcion =="1":
         filtrado = input("Ingrese una palabra clave para filtrar por nombre")
-        filtrar_archivo(filtrado)
+        try:
+            res = filtrar_archivo(filtrado)
+            if res :
+                print("Archivo creado exitosamente!!")
+            else:
+                print("No se han encontrado juegos con ese nombre: ")
+
+        except:
+            print("No se ha logrado crear el archivo")
+
     elif opcion == "2":
-        consola  = input("Ingrese el nombre de una consola para obtener el promedio")
-        promedio_precio_consola(consola)
+        consola  = input("Ingrese el nombre de una consola para obtener el promedio: ")
+        res = promedio_precio_consola(consola)
+
+        if res:
+            print("wiii")
+        else:
+            print("boo")
+
     elif opcion =="3":
-        print("tercera")
+        anio = input("Ingrese año para buscar juegos: ")
+        res = filtrar_juegos_por_anio(anio)
